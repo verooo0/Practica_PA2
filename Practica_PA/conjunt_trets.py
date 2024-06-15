77dd78f84acdae8b59906ad57f38eacbf5658fbb
verooo0 <veronica.onate@estudiantat.upc.edu>
2024-06-15 12:30:34 +0200

    Add files via upload

---
+++ Practica_PA/conjunt_trets.py
@@ -0,0 +1,51 @@
+from arbre_binari_amb_nodes import ArbreBinari
+from parcrom import Parcrom
+from conjunt_indiv import Cjt_individu
+from individu import individu
+import copy as cp
+
+class Cjt_trets:   
+    class _tretsstr: 
+        def _init_(self): 
+            self._parcrom = Parcrom()
+            self._individus = set()
+            
+    def _init_(self): 
+        self.__trets = dict()
+
+    def afegir_tret(self, a, ind, n): 
+        # a es un string, ind es Individu, n és int
+        if a in self.__trets: 
+            if ind in self.__trets[a]._individus:
+                print("  error")
+            else:
+                self.__trets[a]._individus.add(n)
+                self.__trets[a]._parcrom.interseccio_tret(ind.cromosomes())
+        else: 
+            elem = self._tretsstr()
+            elem._individus.add(n)
+            elem._parcrom = cp.deepcopy(ind.cromosomes())
+            self.__trets[a] = elem
+
+    def esborrar_tret(self, n, p, cjt): 
+        if p in self.__trets: 
+            self.__trets[p]._individus
+            if not self.__trets[p]._individus:
+                del self.__trets[p]
+            else:
+                ll = list(self.__trets[p]._individus)
+                ll.sort()
+    
+
+    def consulta_tret(self, nom_tret):
+        if nom_tret in self.__trets:
+            for inv in self.__trets[nom_tret]._individus:
+                print(f"Individu {inv} té el tret {nom_tret}")
+
+        else:
+            print("  error")
+
+
+    def distribucio_tret(self, nom_tret):
+        count = len(self.__trets.get(nom_tret, []))
+        print(f"{nom_tret}: {count}")
