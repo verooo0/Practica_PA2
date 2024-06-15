cba1f1516b37474f6971d323f5ae8d2eb9fd16c1
verooo0 <veronica.onate@estudiantat.upc.edu>
2024-06-15 12:59:27 +0200

    Update main_program.py

--- Practica_PA/main_program.py
+++ Practica_PA/main_program.py
@@ -7,6 +7,7 @@ instruccio = item()
 
 while instruccio != 'fi':
     if instruccio == 'experiment':
+        #print la instruccio aqui o dentro de las funciones(esto para todas las instrucciones)
         numero_individu = int(item())
         numero_cromosomes = int(item())
         experiment_actual = Cjt_individu(numero_individu)
@@ -18,12 +19,14 @@ while instruccio != 'fi':
         individu = int(item())
 
         trets_actuals.afegir_tret(tret,individu, individu)
+        experiment_actual.afegir_tret(tret,individu) #o llamar esta funcion dentro de la funcion de cjt_trets
     
     elif instruccio == 'treure_tret': # només grups 3 persones
         tret = item()
         individu = int(item())
         cjt = item()
         trets_actuals.esborrar_tret(individu, tret, cjt)
+        experiment_actual.esborrar_tret(tret,individu)
 
     elif instruccio == 'consulta_tret':
         nom_tret = item()
