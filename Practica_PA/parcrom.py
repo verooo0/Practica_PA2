57098ae2070761a31ca6a06b668cc5fec736e697
paulaa2 <162131464+paulaa2@users.noreply.github.com>
2024-06-16 11:25:46 +0200

    Update parcrom.py

--- Practica_PA/parcrom.py
+++ Practica_PA/parcrom.py
@@ -1,23 +1,32 @@
 class Parcrom: 
 
-    def __init__ (self, cromosomes=""):
-        self.__num_cromosomes = len(cromosomes)//2
+    def __init__(self, cromosomes=""):
+        self.__num_cromosomes = len(cromosomes) // 2
         self.__cromosomes_sup = cromosomes[:self.__num_cromosomes]
         self.__cromosomes_inf = cromosomes[self.__num_cromosomes:]
 
+    def get_num_cromosomes(self):
+        return self.__num_cromosomes
+
+    def get_cromosomes_sup(self):
+        return self.__cromosomes_sup
+
+    def get_cromosomes_inf(self):
+        return self.__cromosomes_inf
+
     def interseccio_tret(self, nou_parell):
         if self.__num_cromosomes == 0:
-            self.__num_cromosomes = nou_parell.__num_cromosomes
-            self.__cromosomes_sup = nou_parell.__cromosomes_sup
-            self.__cromosomes_inf = nou_parell.__cromosomes_inf
+            self.__num_cromosomes = nou_parell.get_num_cromosomes()
+            self.__cromosomes_sup = nou_parell.get_cromosomes_sup()
+            self.__cromosomes_inf = nou_parell.get_cromosomes_inf()
         else:
             nou_superior = ""
             nou_inferior = ""
             for i in range(self.__num_cromosomes):
-                if self.__cromosomes_sup[i] != nou_parell.__cromosomes_sup[i]:
+                if self.__cromosomes_sup[i] != nou_parell.get_cromosomes_sup()[i]:
                     nou_superior += "-"
                     nou_inferior += "-"
-                elif self.__cromosomes_inf[i] != nou_parell.__cromosomes_inf[i]:
+                elif self.__cromosomes_inf[i] != nou_parell.get_cromosomes_inf()[i]:
                     nou_superior += "-"
                     nou_inferior += "-"
                 else:
@@ -30,6 +39,11 @@ class Parcrom:
         self.__num_cromosomes = 0
         self.__cromosomes_sup = ""
         self.__cromosomes_inf = ""
+
+    def escriure_parcrom(self):
+        print(f"  {self.__cromosomes_sup}")
+        print(f"  {self.__cromosomes_inf}")
+
 
     def escriure_parcrom(self):
         print(f"  {self.__cromosomes_sup}")
