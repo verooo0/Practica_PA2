c22921ef299998a8323b681f09f13ff712f17923
paulaa2 <162131464+paulaa2@users.noreply.github.com>
2024-06-16 10:55:06 +0200

    Update individu.py

--- Practica_PA/individu.py
+++ Practica_PA/individu.py
@@ -4,21 +4,25 @@ class individu:
     def __init__(self, crom): 
         self.__parcrom = Parcrom(crom)
         self.__trets = set()
+        self.__cromosomes = crom
     
-    def consultar_tret(self, t): 
-        return t in self.__trets
+    def consultar_tret(self, a): 
+        return a in self.__trets
     
-    def afegir_tret(self, t): 
-        self.__trets.add(t)
+    def afegir_tret(self, a): 
+        self.__trets.add(a)
     
-    def esborrar_tret(self, t): 
-        self.__trets.remove(t)
+    def esborrar_tret(self, a): 
+        self.__trets.remove(a)
 
     def cromosomes(self): 
         return self.__parcrom
     
     def llegir_individu(self, n): 
         self.__parcrom.llegir_cromosoma(n)
+    
+    def crom(self): 
+        return self.__cromosomes
 
     def escriure_ind(self): 
         self.__parcrom.escriure_parcrom()
@@ -26,5 +30,4 @@ class individu:
         if ll is not None: 
             ll.sort()
             for e in ll: 
-                print('  ', e, sep = '')
-    
+                print('  ', e, sep = '')    
