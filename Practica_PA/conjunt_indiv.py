6ad88eb3ba00c21ace4b88ae1ce07685a6fba18c
verooo0 <veronica.onate@estudiantat.upc.edu>
2024-06-16 19:42:45 +0200

    Update conjunt_indiv.py

--- Practica_PA/conjunt_indiv.py
+++ Practica_PA/conjunt_indiv.py
@@ -8,29 +8,29 @@ class Cjt_individu:
         self.__individus = [None]                    #crear llista amb None per a que el número de l'individu coincideix-hi amb la seva posició a la llista
         self.__arbre = self.__llegir_arbre()         #llegir l'arbre en preorde i marca 0
         for _ in range(n):
-            self.afegir_individu(item())             #cridar al métode afegir_individu amb els cromosomes com input
+            self.afegir_individu(item())             #cridar al mètode afegir_individu amb els cromosomes com input
     
     def consultar_individu(self, ind):
-        return self.__individus[ind].escriure_ind()     #crida al métode escriure_ind de la classe individu 
+        return self.__individus[ind].escriure_ind()     #crida al mètode escriure_ind de la classe individu 
     
     def afegir_individu(self, cromosomes):
         self.__individus.append(individu(cromosomes))    #afageix a la llista d'individus la informació d'aquest( número y cromosomes) que s'ha obtingut després d'inicialitzar la classe individu
     
     def afegir_tret(self, t, n):
-        self.__individus[n].afegir_tret(t)              #crida al métode afegir_tret de la classe individu 
+        self.__individus[n].afegir_tret(t)              #crida al mètode afegir_tret de la classe individu 
 
     def esborrar_tret(self, t, n): 
-        self.__individus[n].esborrar_tret(t)             #crida al métode esborrar_tret de la classe individu
+        self.__individus[n].esborrar_tret(t)             #crida al mètode esborrar_tret de la classe individu
 
     def distribucio(self, tr): 
-        alpha = self.__arbre_distribucio(self.__arbre, tr)      #
-        if alpha is None: 
+        sub_arbre = self.__arbre_distribucio(self.__arbre, tr)      #crida al mètode __arbre_distribucio de la classe cjt_individu
+        if sub_arbre is None:                                       #si el sub_arbre és None vol dir que cap individu te el tret
             return []
         else: 
-            return alpha.inordre()
+            return sub_arbre.inordre()                              #retorna el sub_arbre dels individus que tenen el tret en inordre
           
     def obtenir_individu(self, n):
-        return self.__individus[n]
+        return self.__individus[n]                                  #retorna la informació guardada de l'individu en la llista dels conjunts d'individus
 
         
     def __llegir_arbre(self, marca = 0):
