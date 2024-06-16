15b04bde0ed0d86381c9c3ffddf6768be2480d00
paulaa2 <162131464+paulaa2@users.noreply.github.com>
2024-06-16 10:55:38 +0200

    Update main_program.py

--- Practica_PA/main_program.py
+++ Practica_PA/main_program.py
@@ -1,53 +1,51 @@
-from pytokr import pytokr
+from pytokr import item
 from conjunt_indiv import Cjt_individu
 from conjunt_trets import Cjt_trets
 
-item = pytokr()
 instruccio = item()
-
 while instruccio != 'fi':
     if instruccio == 'experiment':
-        #print la instruccio aqui o dentro de las funciones(esto para todas las instrucciones)
-        numero_individus = int(item())
+        numero_individu = int(item())
+        experiment_actual = Cjt_individu(numero_individu)
+        trets_actuals = Cjt_trets(experiment_actual)
         numero_cromosomes = int(item())
-        experiment_actual = Cjt_individu(numero_individus)
-        trets_actuals = Cjt_trets()
-        print(f"experiment {numero_individus} {numero_cromosomes}")
-        
+        print(f"experiment {numero_individu} {numero_cromosomes}")
 
     elif instruccio == 'afegir_tret':
         tret = item()
-        individu = int(item())
-        print(f"afegir_tret {tret} {individu}")
-        trets_actuals.afegir_tret(tret,individu, individu)
-        experiment_actual.afegir_tret(tret,individu) #o llamar esta funcion dentro de la funcion de cjt_trets
-       
+        individu_num = int(item())
+        print(f"afegir_tret {tret} {individu_num}")
+        individu = experiment_actual.obtenir_individu(individu_num)
+        trets_actuals.afegir_tret(tret, individu, individu_num)
     
-    elif instruccio == 'treure_tret': # només grups 3 persones
+    elif instruccio == 'treure_tret':
         tret = item()
-        individu = int(item())
-        #cjt = item()  no se puede leer 3 cosas pq esta ya sera la siguiente instruccion
-        print(f"treure_tret {tret} {individu}")
-        trets_actuals.esborrar_tret(individu, tret, cjt) #pq tiene tres inputs??
-        experiment_actual.esborrar_tret(tret,individu)
-        
+        individu_num = int(item())
+        individu = experiment_actual.obtenir_individu(individu_num)
+        print(f"treure_tret {tret} {individu_num}")
+        trets_actuals.esborrar_tret(tret, individu_num, individu)
 
     elif instruccio == 'consulta_tret':
         nom_tret = item()
         print(f"consulta_tret {nom_tret}")
         trets_actuals.consulta_tret(nom_tret)
         
-        
     elif instruccio == 'consulta_individu':
-        individu = int(item())
-        print(f"consulta_individu {individu}")
-        experiment_actual.consultar_individu(individu)
-        
+        n = int(item())
+        print(f"consulta_individu {n}")
+        if n < 1 or n > numero_individu: 
+            print(" error")
+        else: 
+            experiment_actual.consultar_individu(n)
 
     elif instruccio == 'distribucio_tret':
         nom_tret = item()
         print(f"distribucio_tret {nom_tret}")
         trets_actuals.distribucio_tret(nom_tret)
+
+    instruccio = item()
+    
+print("fi")
         
 
 
